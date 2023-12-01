78fa39a58f863630e4b58687f699ebf1688fb369
Joshua Hendricks <hendricksjc@gmail.com>
2023-12-01 08:28:25 -0700

    Added Day1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    
+    fun addTwoNums(input: String): Int {
+        var lo: Char? = null
+        var hi: Char? = null
+
+        for (ch in input) {
+            if (ch.isDigit()) {
+                if (lo == null) {
+                    lo = ch
+                }
+                hi = ch
+            }
+        }
+        return "$lo$hi".toInt()
     }
+
+    fun part1(input: List<String>): Int {
+        val sum = input.map { addTwoNums(it) }.sum()
+        return sum
+    }
+
+
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,9 +28,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
