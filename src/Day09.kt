6975afd12170e0aa803d9a4800ad0e4c2cc705f7
Joshua Hendricks <hendricksjc@gmail.com>
2023-12-09 12:19:28 -0700

    Update Day09, finished part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -7,7 +7,7 @@ fun main() {
         return row
     }
 
-    fun nextValue(readings: List<Long>, nextMap : MutableMap<Pair<Long, Long>, Long>, operation: (Long, Long)-> Long): Long {
+    fun nextValue(readings: List<Long>, nextMap : MutableMap<Pair<Long, Long>, Long>, operation: (Long, Long)-> Long, order: (Long, Long) -> Long): Long {
         if (readings.filter { it == 0L }.size == readings.size) {
             //when they are all 0, return next 0
             return 0L
@@ -19,8 +19,8 @@ fun main() {
         }
 
         val nextRow = readings.windowed(2, 1)
-            .map { it[0] - it[1] }
-        val valueBelow = nextValue(nextRow, nextMap, operation)
+            .map { order(it[0], it[1]) }
+        val valueBelow = nextValue(nextRow, nextMap, operation, order)
         val nextValue = operation(first, valueBelow)
         nextMap += (first to second) to nextValue
 
@@ -30,19 +30,20 @@ fun main() {
     fun part1(inputs: List<String>): Long {
         val readings = inputs.map { parseSensorReading(it) }
 
-        val total = readings.sumOf{
-            val nextMap : MutableMap<Pair<Long, Long>, Long> = mutableMapOf()
-            nextValue(it, nextMap) {a, b -> a + b} }
-
+        val total = readings.sumOf {
+            val nextMap: MutableMap<Pair<Long, Long>, Long> = mutableMapOf()
+            nextValue(it, nextMap, { a, b -> a + b }, { a, b -> a - b })
+        }
         return total
     }
 
     fun part2(inputs: List<String>): Long {
         val readings = inputs.map { parseSensorReading(it) }
 
-        val total = readings.sumOf{
-            val nextMap : MutableMap<Pair<Long, Long>, Long> = mutableMapOf()
-            nextValue(it.reversed(), nextMap) {a, b -> a - b} }
+        val total = readings.sumOf {
+            val nextMap: MutableMap<Pair<Long, Long>, Long> = mutableMapOf()
+            nextValue(it.reversed(), nextMap, { a, b -> a - b }, { a, b -> b - a })
+        }
 
         return total
     }
@@ -50,25 +51,14 @@ fun main() {
     test( "Part 1 test", "Day09_test", 114L,  ) {i -> part1(i)}
     test( "Part 1 - 2 test", "Day09_test1-2", -1586L,  ) {i -> part1(i)}
 
-
     println("--- Part 1 ---")
     val input = readInput("Day09")
     part1(input).println()
 
     test( "Part 2 test", "Day09_test2", 5L,  ) {i -> part2(i)}
-//    println("--- Part 2 test ---")
-//    val testInput2 = readInput("Day09_test2")
-//    val part2Answer = part2(testInput2)
-//    val part2Solution = 6L
-//    if (part2Answer != part2Solution) {
-//        error("Part 2 test failed = $part2Answer != $part2Solution")
-//        return
-//    } else {
-//        println("Successful")
-//    }
 
-//    println("--- Part 2 ---")
-//    part2(input).println()
+    println("--- Part 2 ---")
+    part2(input).println()
 }
 
 
