76099d7d4d7f2bbc1ecda1c718d36a8d632d4029
Joshua Hendricks <hendricksjc@gmail.com>
2023-12-11 13:24:59 -0700

    Update Day10, to use tailrec, cleanup part 2, will need to do it again later

--- src/Day10.kt
+++ src/Day10.kt
@@ -15,59 +15,35 @@ fun main() {
     var pathCount: MutableList<MutableList<Int?>> = mutableListOf()
     //row column
 
-    fun findCharPath(inputArray: List<List<Char>>, currentRow: Pair<Int, Int>, goingInDirection: Direction, previousRow: Pair<Int, Int>, awayFromS: Int): Int {
-        var row = currentRow.first
-        var col = currentRow.second
-        var currentRow = currentRow
-        var goingInDirection = goingInDirection
-        var awayFromS = awayFromS
+    tailrec fun findCharPath(inputArray: List<List<Char>>, currentRow: Pair<Int, Int>, goingInDirection: Direction, awayFromS: Int): Int {
+        val row = currentRow.first
+        val col = currentRow.second
         if (!(row >= 0 && row < inputArray.size) ||
             !(col >= 0 && col < inputArray[0].size)) {
             //stop from going out of bounds with check
             return 0
         }
-        var char = inputArray[row][col]
+        val char = inputArray[row][col]
         if (char == '.') {
             pathCount[row][col] = 0
             return 0
         }
+        if (char == 'S') {
+            return awayFromS
+        }
+
         var nowGoingInDirection = directionMap.get(char)?.get(goingInDirection)
             ?: //Not a valid direction on this tile, but don't need to change pathCount
             return 0
 
-        //this is a good direction, follow it
+        val newCoordinate = when (nowGoingInDirection) {
+            Direction.NORTH -> currentRow.first -1 to currentRow.second
+            Direction.SOUTH -> currentRow.first + 1 to currentRow.second
+            Direction.EAST -> currentRow.first to currentRow.second + 1
+            Direction.WEST -> currentRow.first to currentRow.second - 1
 
-        while (char != 'S') {
-            //determine if the direction we are going on block makes sense
-            nowGoingInDirection = directionMap.get(char)?.get(goingInDirection)
-                ?: //Not a valid direction on this tile, dead-end
-                return 0
-
-            //this is a valid direction, so we've gotten here before if there is a count
-            if (pathCount[row][col] != null) {
-                //return the value we have now plus the pathCount
-                return pathCount[row][col]!! + awayFromS
-            }
-
-            //add current pathCount to the list
-            pathCount[row][col] = awayFromS
-
-            //no count yet for this tile, add it and move to the tile direction is telling us
-            val newCoordinate = when (nowGoingInDirection) {
-                Direction.NORTH -> currentRow.first -1 to currentRow.second
-                Direction.SOUTH -> currentRow.first + 1 to currentRow.second
-                Direction.EAST -> currentRow.first to currentRow.second + 1
-                Direction.WEST -> currentRow.first to currentRow.second - 1
-            }
-
-            char = inputArray[newCoordinate.first][newCoordinate.second]
-            row = newCoordinate.first
-            col = newCoordinate.second
-            currentRow = newCoordinate
-            goingInDirection = nowGoingInDirection
-            awayFromS += 1
         }
-        return awayFromS
+        return findCharPath(inputArray, newCoordinate, nowGoingInDirection, awayFromS + 1)
     }
 
 
@@ -82,14 +58,13 @@ fun main() {
         val sCoordinateList = inputs.mapIndexed { rowIndex, row -> row.mapIndexed { colIndex, col -> (col == 'S') to (rowIndex to colIndex) }.filter { it.first }}.find { it.size == 1 }!!
         val sCoordinate = sCoordinateList[0].second
 
-
         val nextDirections = listOf(
             (sCoordinate.first+1 to sCoordinate.second) to Direction.SOUTH,
             (sCoordinate.first-1 to sCoordinate.second) to Direction.NORTH,
             (sCoordinate.first to sCoordinate.second + 1) to Direction.EAST,
             (sCoordinate.first to sCoordinate.second - 1) to Direction.WEST,
         )
-        val sums = nextDirections.map { findCharPath(array, it.first, it.second, sCoordinate, 1) }
+        val sums = nextDirections.map { findCharPath(array, it.first, it.second,  1) }
         pathCount
             .map { it -> it.map { it ?: 0 }}
             .forEach{
@@ -99,8 +74,9 @@ fun main() {
 
     }
 
+
     fun part2(inputs: List<String>): Long {
-        return 0
+        return 0L
     }
 
     test( "Part 1 test 1 - 1", "Day10_test1-1", 4L,  ) {i -> part1(i)}
@@ -110,7 +86,7 @@ fun main() {
     val input = readInput("Day10")
     part1(input).println()
 
-//    test( "Part 2 test", "Day10_test2", 5L,  ) {i -> part2(i)}
+    test( "Part 2 test", "Day10_test2", 8L,  ) {i -> part2(i)}
 //
 //    println("--- Part 2 ---")
 //    part2(input).println()
